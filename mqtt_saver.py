c1120a592f98aa055c5f5c565f10d644613da7c7
fsck <inaforloop@gmail.com>
2024-11-24 12:33:44 -0500

    seems to be working

--- mqtt_saver.py
+++ mqtt_saver.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
 
+import os
 import sys
 import argparse
 import subprocess
+import logging
+
+from typing import Any
 
 import paho.mqtt.client as mqtt
-from paho.mqtt.client import MQTTMessage
+from paho.mqtt.client import MQTTMessage, ConnectFlags, DisconnectFlags
+from paho.mqtt.enums import CallbackAPIVersion
+from paho.mqtt.reasoncodes import ReasonCode
+from paho.mqtt.properties import Properties
 
 DEF_MQTT_SERVER = "10.0.0.2"
-DEF_MQTT_PORT = 1883
 
 
 class ShellExecError(Exception):
@@ -38,17 +44,57 @@ def exec(cmd_line: str, cwd: str | None = None, check: bool = True) -> tuple[int
 def start_screen_saver() -> None:
 
     cmd_line = "xset dpms force off"
-    exec(cmd_line)
+    ret, stdout, stderr = exec(cmd_line, check=False)
+
+    logging.info(f"{cmd_line} -> {ret}")
+
+    if 0 != ret:
+        err_msg = f"{cmd_line} returned {ret}"
+        err_msg += f"stdout={stdout} stderr={stderr}"
+        logging.error(err_msg)
+
+
+def on_connect(client: mqtt.Client, userdata: Any, connect_flags: ConnectFlags, reason_code: ReasonCode, properties: Properties | None) -> None:
+    logging.info(f"connected. reason={reason_code}")
+
+
+def on_disconnect(client: mqtt.Client, userdata: Any, disconnect_flags: DisconnectFlags, reason_code: ReasonCode, props: Properties | None) -> None:
+    logging.info(f"disconnected. reason={reason_code}")
 
 
 def on_message(client: mqtt.Client, userdata: str, msg: MQTTMessage) -> None:
 
     payload = msg.payload.decode("utf-8")
 
+    logging.info(f"{msg.topic} -> {payload}")
+
     if "away" == payload:
         start_screen_saver()
     else:
         print(f"Unknown payload \"{payload}\"")
+
+
+def init_logging(verbose: bool, file_name: str = "logs.log"):
+
+    script_root = os.path.abspath(os.path.dirname(sys.argv[0]))
+    log_file = os.path.join(script_root, file_name)
+
+    formatter = logging.Formatter(
+        '%(created)-18s - %(levelname)s - %(message)s')
+
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    file_handler = logging.FileHandler(log_file)
+    file_handler.setLevel(logging.INFO)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
+    if verbose:
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.INFO)
+        console_handler.setFormatter(formatter)
+        logger.addHandler(console_handler)
 
 
 def main() -> int:
@@ -63,19 +109,22 @@ def main() -> int:
                         default=DEF_MQTT_SERVER,
                         help=f"MQTT server. Default: {DEF_MQTT_SERVER}")
 
-    parser.add_argument("-p",
-                        "--port",
-                        type=int,
-                        default=DEF_MQTT_PORT,
-                        help=f"MQTT server port. Default: {DEF_MQTT_PORT}")
+    parser.add_argument("-v",
+                        "--verbose",
+                        action="store_true",
+                        help="verbose")
 
     try:
         args = parser.parse_args()
 
-        client = mqtt.Client()
-        client.connect(args.server, args.port)
-        client.on_message = on_message
+        init_logging(args.verbose)
+
+        client = mqtt.Client(CallbackAPIVersion.VERSION2)
+        client.connect(args.server, keepalive=10)
         client.subscribe("/motion/office")
+        client.on_message = on_message
+        client.on_connect = on_connect
+        client.on_disconnect = on_disconnect
         client.loop_forever()
 
         status = 0
